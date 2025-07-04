a487eea94f97108d81cef141a42583c8a6addb2b
nirut yodjai <nirutyodjai@gmail.com>
2025-07-04 17:11:41 +0000

    นำโค้ดที่พึ่งใส่ไปออก แล้วนำแค่โค้ดไปใส่ อย่างเช่นไม่ต้องใส่ชื่อในเมนูแล

--- src/components/sidebar/reaction-buttons.tsx
+++ src/components/sidebar/reaction-buttons.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+
+const reactions = [
+    { name: 'Like', emoji: '👍' },
+    { name: 'Cheer', emoji: '👏🏻' },
+    { name: 'Celebrate', emoji: '🎉' },
+    { name: 'Appreciate', emoji: '✨' },
+    { name: 'Smile', emoji: '🙂' },
+];
 
 const ReactionButtons = () => {
   return (
-    <div className="hover:scale-x-105 transition-all duration-300 *:transition-all *:duration-300 flex justify-start text-2xl items-center shadow-xl z-10 bg-[#e8e4df] dark:bg-[#191818] gap-2 p-2 rounded-full">
-      <button className="relative before:hidden hover:before:flex before:justify-center before:items-center before:h-4 before:text-[.6rem] before:px-1 before:content-['Like'] before:bg-black dark:before:bg-white dark:before:text-black before:text-white before:bg-opacity-50 before:absolute before:-top-7 before:rounded-lg hover:-translate-y-5 cursor-pointer hover:scale-125 bg-white dark:bg-[#191818] rounded-full p-2 px-3">
-        👍
-      </button>
-      <button className="relative before:hidden hover:before:flex before:justify-center before:items-center before:h-4 before:text-[.6rem] before:px-1 before:content-['Cheer'] before:bg-black dark:before:bg-white dark:before:text-black before:text-white before:bg-opacity-50 before:absolute before:-top-7 before:rounded-lg hover:-translate-y-5 cursor-pointer hover:scale-125 bg-white dark:bg-[#191818] rounded-full p-2 px-3">
-        👏🏻
-      </button>
-      <button className="relative before:hidden hover:before:flex before:justify-center before:items-center before:h-4 before:text-[.6rem] before:px-1 before:content-['Celebrate'] before:bg-black dark:before:bg-white dark:before:text-black before:text-white before:bg-opacity-50 before:absolute before:-top-7 before:rounded-lg hover:-translate-y-5 cursor-pointer hover:scale-125 bg-white dark:bg-[#191818] rounded-full p-2 px-3">
-        🎉
-      </button>
-      <button className="relative before:hidden hover:before:flex before:justify-center before:items-center before:h-4 before:text-[.6rem] before:px-1 before:content-['Appreciate'] before:bg-black dark:before:bg-white dark:before:text-black before:text-white before:bg-opacity-50 before:absolute before:-top-7 before:rounded-lg hover:-translate-y-5 cursor-pointer hover:scale-125 bg-white dark:bg-[#191818] rounded-full p-2 px-3">
-        ✨
-      </button>
-      <button className="relative before:hidden hover:before:flex before:justify-center before:items-center before:h-4 before:text-[.6rem] before:px-1 before:content-['Smile'] before:bg-black dark:before:bg-white dark:before:text-black before:text-white before:bg-opacity-50 before:absolute before:-top-7 before:rounded-lg hover:-translate-y-5 cursor-pointer hover:scale-125 bg-white dark:bg-[#191818] rounded-full p-2 px-3">
-        🙂
-      </button>
-    </div>
+    <TooltipProvider delayDuration={0}>
+        <div className="hover:scale-x-105 transition-all duration-300 *:transition-all *:duration-300 flex justify-start text-2xl items-center shadow-xl z-10 bg-[#e8e4df] dark:bg-[#191818] gap-2 p-2 rounded-full">
+        {reactions.map((reaction) => (
+            <Tooltip key={reaction.name}>
+                <TooltipTrigger asChild>
+                    <button className="transition-transform duration-300 hover:-translate-y-5 cursor-pointer hover:scale-125 bg-white dark:bg-[#191818] rounded-full p-2 px-3">
+                        {reaction.emoji}
+                    </button>
+                </TooltipTrigger>
+                <TooltipContent>
+                    <p>{reaction.name}</p>
+                </TooltipContent>
+            </Tooltip>
+        ))}
+        </div>
+    </TooltipProvider>
   );
 }
 
